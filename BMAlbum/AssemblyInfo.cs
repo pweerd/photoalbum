fc9996dd04ecb0109ce412e1ee4b3f421d0de864
Peter van der Weerd <pweerd@bitmanager.nl>
2025-04-30 20:10:36 +0200

    Included assembly info

---
+++ BMAlbum/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("Foto/video Album website")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("Bitmanager foto/video Album website")]
+[assembly: AssemblyCompany("De Bitmanager (https://bitmanager.nl)")]
+[assembly: AssemblyCopyright("Copyright © 2024, De Bitmanager")]
+[assembly: AssemblyVersion("2.0.2025.0430")]
+[assembly: AssemblyFileVersion("2.0.2025.0430")]
