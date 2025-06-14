14cf0c627fa65f2d87d43705da985c3ac2a860eb
Peter van der Weerd <pweerd@bitmanager.nl>
2025-06-14 19:59:22 +0200

    - Added AssemblyInfo for BMAlbumTools - Recompiled everything due to updated core modules - Updated sample xml, since ImportEngine supports an IgnoreCanceled-flag

--- BMAlbumTools/AssemblyInfo.cs
+++ BMAlbumTools/AssemblyInfo.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Reflection;
 
-[assembly: AssemblyTitle("Foto/video Album website")]
+[assembly: AssemblyTitle("Common tools for Album website")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("Bitmanager foto/video Album website")]
 [assembly: AssemblyCompany("De Bitmanager (https://bitmanager.nl)")]
 [assembly: AssemblyCopyright("Copyright © 2024, De Bitmanager")]
-[assembly: AssemblyVersion("2.0.2025.0614")]
-[assembly: AssemblyFileVersion("2.0.2025.0614")]
+//NO_UPDATE_ASSEMBLY_VERSION
+[assembly: AssemblyVersion("2.0.2025.0610")]
+[assembly: AssemblyFileVersion("2.0.2025.0610")]
